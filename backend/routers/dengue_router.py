c51cc64ab85507efc52842fe7393efced00a9b6f
Victor Rambo <victor.rambo.dev@gmail.com>
2026-07-06 21:02:55 -0300

    feat(api): add GET /cases/forecast endpoint

---
+++ backend/routers/dengue_router.py
@@ -0,0 +1,94 @@
+from fastapi import APIRouter, Query
+from typing import List
+from pydantic import BaseModel
+from backend.services import factory
+
+router = APIRouter()
+
+
+class DengueCaseResponse(BaseModel):
+    notification_date: int 
+    first_symptoms_date: int
+    epidemiological_week: int
+    
+    state_notification_code: int
+    city_notification_code: int
+
+    state_living_code: int
+    city_living_code: int
+
+    age: int
+    year_birth: int
+    escolarity: int
+    profession: int
+    pregnacy_state: int
+    ethnicity: int
+    sex: str 
+    
+    class Config:
+        from_attributes = True
+
+
+class DengueNumCasesResponse(BaseModel):
+    date_ym: int 
+    num_cases: int
+
+
+_page_max_size = 500
+_city_code_description = "**Filter:** Brazilian City of Occurrence \n- **Format:** `Six first digits of IBGE city code identifier`"
+_start_date_description = "**Filter:** Start Date \n - **Format:**: `YYYYMMDD`"
+_end_date_description = "**Filter:** End Date \n - **Format:**: `YYYYMMDD`"
+
+
+@router.get("/cases/city-date", response_model=List[DengueCaseResponse])
+def get_cases(city_code: int = Query(description=_city_code_description), 
+              start_date: int = Query(description=_start_date_description), 
+              end_date: int = Query(description=_end_date_description),
+              page: int = 1,
+              page_size: int = _page_max_size):
+    
+    page_size = min(page_size, _page_max_size)
+    offset = (page - 1) * page_size
+
+    dataset_searcher = factory.create_dengue_dataset_searcher()
+    cases = dataset_searcher.get_cases_dates(start_date, end_date, city_code)
+
+    if not cases:
+        return []
+
+    response = cases[offset:offset+page_size]
+
+    return response
+
+
+@router.get("/cases/forecast", response_model=List[DengueNumCasesResponse])
+def get_cases(city_code: int = Query(description=_city_code_description)):
+    
+    dataset_forecaster = factory.create_dengue_dataset_forecaster()
+    date_to_forecast = dataset_forecaster.get_dengue_forecast(city_code)
+    
+    response = []
+    for date_ym, num_cases_forecast in date_to_forecast.items():
+        response.append(DengueNumCasesResponse(date_ym=date_ym, num_cases=num_cases_forecast))
+
+    return response
+
+
+@router.get("/num-cases/city-date", response_model=List[DengueNumCasesResponse])
+def get_cases(city_code: int = Query(description=_city_code_description), 
+              start_date: int = Query(description=_start_date_description), 
+              end_date: int = Query(description=_end_date_description)):
+    
+    dataset_searcher = factory.create_dengue_dataset_searcher()
+    num_monthly_cases = dataset_searcher.get_num_cases_dates(start_date, end_date, city_code)
+
+    if not num_monthly_cases:
+        return []
+    
+    response = []
+    for date, num_cases in num_monthly_cases.items():
+        response.append(DengueNumCasesResponse(date_ym=date, num_cases=num_cases))
+
+    return response
+
+
--- main.py
+++ main.py
@@ -1,33 +1,14 @@
 
 
-from backend.services import factory
 from fastapi import FastAPI
-from backend.routers import dengue_cases
+from backend.routers import dengue_router
+
 
 app = FastAPI()
 
 app.include_router(
-    dengue_cases.router,
+    dengue_router.router,
     prefix="/api/dengue",
     tags=["Arbovirus - Dengue"])
 
 
-
-"""dataset_importer = factory.create_dengue_dataset_importer()
-dataset_importer.import_years(20230101, 20261201)
-
-dataset_storer = factory.create_dengue_dataset_storer()
-dataset_storer.store_years(20230101, 20261201)
-"""
-
-dataset_builder = factory.create_dengue_dataset_builder()
-dataset_builder.build_years(20230302, 20260702)
-
-dataset_searcher = factory.create_dengue_dataset_searcher()
-cases = dataset_searcher.get_cases_dates(20260417, 20260615, 320530)
-
-for case in cases:
-    print(case.notification_date)
-
-date = dataset_searcher.get_num_cases_dates(20260417, 20260615, 320530)
-print(date)

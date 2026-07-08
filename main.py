eeaef5c6ca9d0515b6f343a94b36c05723ec1bbd
Victor Rambo <victor.rambo.dev@gmail.com>
2026-07-08 11:41:18 -0300

    docs: add title and description to FastApi app

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from backend.routers import dengue_router
 
-app = FastAPI()
+app = FastAPI(
+    title="Dengue Data Engine",
+    description=(
+        "A data engine and REST API designed to ingest, organize, store, "
+        "and distribute official dengue epidemiological data from the "
+        "Brazilian Ministry of Health Open Data Portal (SINAN/Dengue)."
+    ),
+    version="1.0.0",
+)
 
 app.include_router(
     dengue_router.router,
